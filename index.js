65bbdcbb36dbba80d55239062d523e2a6668195a
Timothy Pidashev <pidashev.tim@gmail.com>
2024-05-22 12:20:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,6 @@ function generateProgressBar() {
 
 const readme = `\
 
-```c
-#include <stdio.h>
-
-int main(){
-  printf("My name is Timothy Pidashev! I am a 17 year on an epic journey to become a software developer!");
-  printf("Experienced in C, Python, Java, and Javascript")
-  printf("Join my discord server: https://discord.gg/EDRjZdkGBG");
-  return 0;
-}
-```
-
 <!--START_SECTION:waka-->
 
 <!--END_SECTION:waka-->
